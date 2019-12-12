93302e51b50adb7d8752a6f3beed29b27a8811ae
Harumi Warner <harumi@harumis-mbp.home>
2019-12-12 15:12:27 -0500

    cleaned up.

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,4 @@
 import elements from './elements';
-import MoveElement from './MoveElement';
-import ResizeElement from './ResizeElement';
 import Element from './Element';
 import Offsets from './models/Offsets';
 
@@ -15,8 +13,6 @@ offsetDisplayElem.append(newOffsetElemImg);
 const offsetsModelImg = new Offsets(newOffsetElemImg, 'Image Offset');
 
 const agileImage = new Element(imgElement, imgOrigin, canvas, offsetsModelImg, isText).bindEvent();
-// const imgMove = new MoveElement(imgElement, imgOrigin, canvas, offsetsModelImg).bindEvent();
-// const imgResize = new ResizeElement(imgElement, offsetsModelImg, canvas, isText).bindEvent();
 
 // Text Element
 isText = true;
@@ -33,5 +29,3 @@ const agileText = new Element(
   offsetsModelText,
   isText
 ).bindEvent();
-// const textMove = new MoveElement(textElement, textOrigin, canvas, offsetsModelText).bindEvent();
-// const textResize = new ResizeElement(textElement, offsetsModelText, canvas, isText).bindEvent();
